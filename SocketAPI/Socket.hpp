0e66412f14825e170a0376a0e3354aecb357ec01
wangyihub <412461202@qq.com>
2020-03-13 15:12:32 +0800

    添加SocketAPI项目

---
+++ SocketAPI/Socket.hpp
@@ -0,0 +1,106 @@
+#ifndef _SOCKET_HPP_
+#define _SOCKET_HPP_
+
+#include<iostream>
+#include <stdio.h>
+#include<WinSock2.h>
+#pragma comment (lib, "ws2_32.lib")  //加载 ws2_32.dll
+
+namespace Socket
+{
+	constexpr auto MAX_BUFFER = 1024;
+	constexpr auto iMode = 0; //1:非阻塞，0：阻塞
+
+	typedef struct
+	{
+		std::string ip;
+		int port;
+	}Address;
+
+	typedef struct
+	{
+		Address address;
+		std::string data;
+	}Datagram;
+
+	class UDP
+	{
+	public:
+		UDP();
+		~UDP();
+
+		void close(); //释放套接口
+		BOOL bind(int prot); //绑定端口
+		INT send(std::string& ip, const int prot, const std::string& data); //发送数据
+		BOOL receive(Datagram& pack); //接受数据
+
+	private:
+		SOCKET m_socket_server;
+		BOOL m_binded;
+
+		WSADATA m_wsaData;
+	};
+
+	UDP::UDP()
+	{
+		this->m_socket_server = INVALID_SOCKET;
+		this->m_binded = true;
+
+		if (WSAStartup(MAKEWORD(2, 2), &m_wsaData) == 0)
+		//创建套接字
+		m_socket_server = socket(AF_INET, SOCK_DGRAM, 0);
+		if (m_socket_server == INVALID_SOCKET)
+			this->m_binded = false;
+		else
+			if (ioctlsocket(m_socket_server, FIONBIO, (u_long FAR*) & iMode) == -1)
+				this->m_binded = false;
+	}
+
+	UDP::~UDP()
+	{
+	}
+	void UDP::close()
+	{
+		closesocket(m_socket_server);
+		WSACleanup();
+	}
+	BOOL UDP::bind(int prot)
+	{
+		if (!m_binded) return false;
+
+		sockaddr_in servAddr;
+		memset(&servAddr, 0, sizeof(servAddr));  //每个字节都用0填充
+		servAddr.sin_family = PF_INET;  //使用IPv4地址
+		servAddr.sin_addr.s_addr = htonl(INADDR_ANY); //自动获取IP地址
+		servAddr.sin_port = htons(1234);  //端口
+
+		return  m_binded = !::bind(m_socket_server, (SOCKADDR*)&servAddr, sizeof(SOCKADDR));
+	}
+	INT UDP::send(std::string& ip, const int prot, const std::string& data)
+	{
+		sockaddr_in clntAddr;  //客户端地址信息
+		int nSize = sizeof(SOCKADDR);
+		clntAddr.sin_family = PF_INET;
+		clntAddr.sin_addr.s_addr = inet_addr(ip.c_str()); 
+		clntAddr.sin_port = htons(prot);  //端口
+
+		return sendto(m_socket_server, data.c_str(), data.length(), 0, (SOCKADDR*)&clntAddr, nSize);
+	}
+	BOOL UDP::receive(Datagram& pack)
+	{
+		sockaddr_in clntAddr;
+		int nSize = sizeof(SOCKADDR);
+		char buffer[MAX_BUFFER + 1] = { 0 };  //缓冲区
+		//memset(buffer, 0, MAX_BUFFER + 1);
+		if (recvfrom(m_socket_server, buffer, MAX_BUFFER, 0, (SOCKADDR*)&clntAddr, &nSize) == -1)
+			return false;
+
+		pack.data = buffer;
+		pack.address.ip = inet_ntoa(clntAddr.sin_addr);
+		pack.address.port = ntohs(clntAddr.sin_port);
+
+		return true;
+	}
+}
+
+#endif // !_SOCKET_HPP_
---
+++ SocketAPI/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "Socket.hpp"
+#include <thread>
+
+void threadFun(Socket::UDP* udp);
+
+int main()
+{
+	std::string ip = "127.0.0.1";
+	int prot = 2046;
+	std::string str = "Server";
+	Socket::Datagram pack;
+
+	Socket::UDP udp;
+	udp.bind(1024);
+
+	std::thread t(threadFun, &udp);
+
+	while (true)
+	{
+		std::cin >> str;
+		udp.send(ip, prot, str);
+	}
+	
+	t.join();
+	udp.close();
+
+	system("pause");
+	return 0;
+}
+
+void threadFun(Socket::UDP* udp)
+{
+	Socket::Datagram pack;
+	while (true)
+	{
+		if (udp->receive(pack))
+		{
+			std::cout << "IP: " << pack.address.ip << "Prot: " << pack.address.port << std::endl;
+			std::cout << "Date: " << pack.data << std::endl;
+		}
+		std::cout << "-----------------------" << std::endl;
+	}
+}
